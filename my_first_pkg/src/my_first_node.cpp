177cb3792e03cff953a8521fe2dbe529820a0698
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2017-04-12 14:19:54 +0200

    my_first_node now publishes a message.

--- my_first_pkg/src/my_first_node.cpp
+++ my_first_pkg/src/my_first_node.cpp
@@ -1,9 +1,15 @@
 #include "ros/ros.h"
+#include "std_msgs/Float64.h"
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "my_first_node");
+  ros::NodeHandle nh("~");
+  ros::Publisher pub = nh.advertise<std_msgs::Float64>("my_num", 1);
+  ros::Duration(1.0).sleep();
+  std_msgs::Float64 msg;
+  msg.data = -1.23;
+  pub.publish(msg);
   ros::spin();
   return 0;
 }
-
