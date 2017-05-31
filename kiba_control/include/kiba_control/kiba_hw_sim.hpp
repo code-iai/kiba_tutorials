71cef4a0ab817acff2cfecdfe15ee7edfd4a2e5f
Georg Bartels <georg.bartels@cs.uni-bremen.de>
2017-05-31 22:14:07 +0200

    Setting init config of robot control lecture.

--- kiba_control/include/kiba_control/kiba_hw_sim.hpp
+++ kiba_control/include/kiba_control/kiba_hw_sim.hpp
@@ -2,6 +2,7 @@
 #define KIBA_CONTROL_KIBA_HW_SIM_H
 
 #include <ros_control_boilerplate/sim_hw_interface.h>
+#include <kiba_control/utils.hpp>
 
 namespace kiba_control
 {
@@ -9,16 +10,27 @@ namespace kiba_control
   {
     public:
       KibaHwSim(ros::NodeHandle& nh, urdf::Model* urdf_model = NULL) :
-        ros_control_boilerplate::SimHWInterface(nh, urdf_model)
+        ros_control_boilerplate::SimHWInterface(nh, urdf_model), nh_(nh)
       {
-        // FIXME: read default joint values
-  
       }
 
       void init()
       {
         ros_control_boilerplate::SimHWInterface::init();
+
+        // read and set init configuration
+        std::map<std::string, double> init_config =
+          readParam< std::map<std::string, double> >(nh_, "init_config");
+
+        for (size_t i=0; i<joint_names_.size(); ++i)
+        {
+          joint_position_[i] = init_config.find(joint_names_[i])->second;
+          joint_position_command_[i] = joint_position_[i];
+        }
       }
+
+    private:
+      ros::NodeHandle nh_;
   };
 }
 #endif
